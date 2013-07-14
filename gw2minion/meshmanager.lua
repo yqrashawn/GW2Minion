17cd54896cd7dc261ae356931925c04cf83502bb
fxfire <s.karsten@scenix3d.com>
2013-07-14 21:20:31 +0200

    paranoiafix

--- gw2minion/meshmanager.lua
+++ gw2minion/meshmanager.lua
@@ -510,7 +510,7 @@ function c_mapchange:evaluate()
 					if not (wt_core_helpers:IsInPartyList(char.name)) then
 						c_mapchange.tickCount = math.random(30000,90000) + wt_global_information.Now
                         c_mapchange.playerCount = c_mapchange.playerCount + 1
-                        wt_debug("Player detected: "..tostring(char.name).. " - Count: "..tostring(c_mapchange.playerCount))
+                        --wt_debug("Player detected: "..char.name.. " - Count: "..tostring(c_mapchange.playerCount))
                         if (c_mapchange.playerCount >= tonumber(gparanoiaswitchcount)) then
                             wt_debug("Paranoia count exceeded - switching maps")
                             c_mapchange.doSwitch = true
