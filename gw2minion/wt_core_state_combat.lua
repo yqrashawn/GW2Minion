3fc2f7bae078d942704e84b591c6b6cb047bfe83
Jorith <jmzwaan@gmail.com>
2013-10-10 23:46:43 +0200

    fix in ignore list
    simplified some code.

--- gw2minion/wt_core_state_combat.lua
+++ gw2minion/wt_core_state_combat.lua
@@ -53,13 +53,9 @@ function c_combat_over:evaluate()
 			wt_core_state_combat.StopCM()
 		end
 		local T = CharacterList:Get( wt_core_state_combat.CurrentTarget )
-		if (T == nil or not T.alive or not T.onmesh or T.attitude == 0 or T.attitude == 3) then
-			Player:ClearTarget()
-			return true
-		elseif (T ~= nil and wt_global_information.TargetIgnorelist ~= nil and wt_global_information.TargetIgnorelist[T.contentID] ~= nil and wt_global_information.TargetIgnorelist[T.contentID] < T.health.percent) then
-			Player:ClearTarget()
-			return true
-		elseif (wt_global_information.TargetBlacklist ~= nil and wt_global_information.TargetBlacklist[wt_core_state_combat.CurrentTarget] ~= nil) then
+		if (T == nil or not T.alive or not T.onmesh or T.attitude == 0 or T.attitude == 3)
+		or (T ~= nil and wt_global_information.TargetIgnorelist ~= nil and wt_global_information.TargetIgnorelist[T.contentID] ~= nil and wt_global_information.TargetIgnorelist[T.contentID] < T.health.percent)
+		or (wt_global_information.TargetBlacklist ~= nil and wt_global_information.TargetBlacklist[wt_core_state_combat.CurrentTarget] ~= nil) then
 			Player:ClearTarget()
 			return true
 		end
