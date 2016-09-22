772cd48caa1c7f4d39adc1129d600ae3507c95d8
fxfire <minionoffice@gmail.com>
2016-09-22 10:39:29 +0200

    dtdzti

--- GW2Minion/gw2.lua
+++ GW2Minion/gw2.lua
@@ -249,6 +249,7 @@ function gw2minion.ModuleInit()
 		 ml_mesh_mgr.SetDefaultMesh(1011,"sPvP Battle of Champions")
 		 ml_mesh_mgr.SetDefaultMesh(1015,"mmoalt_SilverWastes")
 		 ml_mesh_mgr.SetDefaultMesh(1163,"sPvP Revenge of the Capricorn")
+		 ml_mesh_mgr.SetDefaultMesh(1171,"SPvp Coloseum")
 		 
 		--Instances
 		ml_mesh_mgr.SetDefaultMesh(896,"North Noland Hatchery") -- Diessa Plateau
