e3ea17d5d22423bd526514e7d6f4b690b5c189ef
mmoalt <mmoalt@outlook.com>
2016-10-03 14:40:04 +0200

    tweaks

--- GW2Minion/gw2_globals.lua
+++ GW2Minion/gw2_globals.lua
@@ -58,11 +58,54 @@ ml_global_information.InvulnerabilityConditions = {
 	[36143] = "Destruction Immunity",
 }
 
+-- Moved this here so it's easier to look at and easier to copy paste
+function ml_global_information.OnUpdate(Event,ticks)
+	if(TimeSince(ml_global_information.Lasttick) > ml_bt_mgr.throttletmr) then
+		ml_global_information.Lasttick = ticks
+		
+		ml_global_information.GameState = GetGameState()
+		
+		if(Player) then
+			ml_global_information.Player_ID = Player.id or 0
+			ml_global_information.Player_Name = Player.name or ""
+			ml_global_information.Player_Health = Player.health or { current = 0, max = 0, percent = 0 }
+			ml_global_information.Player_Profession = Player.profession or 0
+			ml_global_information.Player_ProfessionName = table_invert(GW2.CHARCLASS)[ml_global_information.Player_Profession] or "NoClass"
+			ml_global_information.Player_Power = Player.power or 0
+			ml_global_information.Player_Endurance = Player.endurance or 0
+			ml_global_information.Player_InCombat = Player.incombat or false		
+			ml_global_information.Player_Position = Player.pos
+			ml_global_information.Player_Level = Player.level
+			ml_global_information.Player_OnMesh = Player.onmesh or false
+			ml_global_information.Player_Alive = Player.alive or false
+			ml_global_information.Player_IsMoving = Player:IsMoving() or false
+			ml_global_information.Player_CanMove = Player:CanMove() or false
+			ml_global_information.Player_MovementDirections = Player:GetMovement() or { forward=false, backward=false, left=false, right=false }
+			ml_global_information.Player_Inventory_SlotsFree = Inventory.freeslotcount or 0
+			ml_global_information.Player_HealthState = Player.healthstate or 0
+			ml_global_information.Player_SwimState = Player.swimming or 0
+			ml_global_information.Player_MovementState = Player:GetMovementState() or 1
+			ml_global_information.Player_Party = Player:GetParty() or nil
+
+			ml_global_information.CurrentMapID = Player:GetLocalMapID() or 0
+			if (gw2_datamanager and ml_global_information.CurrentMapID ~= 0) then  
+				ml_global_information.CurrentMapName = gw2_datamanager.GetMapName(ml_global_information.CurrentMapID) 
+			else
+				ml_global_information.CurrentMapName = ""
+			end
+
+			ml_global_information.AttackRange = gw2_skill_manager.GetMaxAttackRange()
+			ml_global_information.Player_CurrentWeaponSet = Player:GetCurrentWeaponSet() or 0	-- 0 Aqua1, 1 Aqua2, 2 Engikit, 3 Necro Lich Form/ranger astralform,  4 Weapon1, 5 Weapon2
+			ml_global_information.Player_TransformID = Player:GetTransformID() or 0 -- 1-4 attunement, 5 deathshroud, 9 rangernormal, 10 rangerastralform
+		end
+	end
+end
+RegisterEventHandler("Gameloop.Update", ml_global_information.OnUpdate)
+
 function ml_global_information.Start()
 	gw2_unstuck.Start()
 end
 
--- Stops the Bot
 function ml_global_information.Stop()
     Player:StopMovement()
 	gw2_unstuck.Stop()
