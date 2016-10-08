dc014e090d12f4bfedf503ad196dc00afaf14e54
Jorith <jmzwaan@gmail.com>
2016-10-08 22:36:59 +0200

    - updated global enums for boons and conditions. (removed some non boon/conditions that were wrongly listed) - formatted buff enums to be more readable.

--- GW2Minion/gw2_globals.lua
+++ GW2Minion/gw2_globals.lua
@@ -5,67 +5,69 @@ ml_global_information.Running = false
 ml_global_information.MAX_SKILLBAR_SLOTS = 20
 
 ml_global_information.ConditionsEnum = {
-	[736] = "Bleeding",
-	[720] = "Blind",
-	[737] = "Burning",
-	[722] = "Chilled",
-	[861] = "Confusion",
-	[721] = "Crippled",
-	[791] = "Fear",
-	[727] = "Immobilized",
-	[738] = "Vulnerability",
-	[742] = "Weakness",
-	[723] = "Poison",
-	[890] = "Revealed", --you cannot stealth
-	[872] = "Stun",
-	[27705] = "Taunt",
+	[736]		= "Bleeding",
+	[720]		= "Blind",
+	[737]		= "Burning",
+	[722]		= "Chilled",
+	[861]		= "Confusion",
+	[721]		= "Crippled",
+	[791]		= "Fear",
+	[727]		= "Immobilized",
+	[738]		= "Vulnerability",
+	[742]		= "Weakness",
+	[723]		= "Poison",
+	[27705]		= "Taunt",
+	[26766]		= "Slow",
+	[19426]		= "Torment",
 };
 
 ml_global_information.BoonsEnum = {
-	[743] = "Aegis",
-	[725] = "Fury",
-	[740] = "Might",
-	[717] = "Protection",
-	[718] = "Regeneration",
-	[873] = "Retaliation",
-	[1122] = "Stability",
-	[719] = "Swiftness",
-	[726] = "Vigor",
-	[762] = "Determined",
-	[5979] = "Heal Turret Toggle",
-	[5891] = "Thumper Toggle",
+	[743]		= "Aegis",
+	[17675]		= "Aegis",
+	[725]		= "Fury",
+	[740]		= "Might",
+	[717]		= "Protection",
+	[1187]		= "Quickness",
+	[718]		= "Regeneration",
+	[17674]		= "Regeneration",
+	[26980]		= "Resistance",
+	[873]		= "Retaliation",
+	[1122]		= "Stability",
+	[719]		= "Swiftness",
+	[726]		= "Vigor",
+	[762]		= "Determined",
 };
 
 ml_global_information.SpeedBoons = {
-	[719] = "Swiftness",
-	[5974] = "Super Speed",
-	[5543] = "Mist form",
-	[12542] = "Signet of the Hunt",
-	[13060] = "Signet of Shadows",
-	[5572] = "Signet of Air",
-	[10612] = "Signet of the Locust"
+	[719]		= "Swiftness",
+	[5974]		= "Super Speed",
+	[5543]		= "Mist form",
+	[12542]		= "Signet of the Hunt",
+	[13060]		= "Signet of Shadows",
+	[5572]		= "Signet of Air",
+	[10612]		= "Signet of the Locust",
 }
 
 ml_global_information.SlowConditions = {
-	[721] = "Cripple",
-	[722] = "Chill",
-	[18621] = "Ichor"
+	[721]		= "Cripple",
+	[722]		= "Chill",
+	[18621]		= "Ichor",
 }
 
 ml_global_information.ImmobilizeConditions = {
-	[727] = "Immobilize",
-	[791] = "Fear",
-	[872] = "Stun",
-	[833] = "Daze",
-	[27705] = "Taunt",
-	[15090] = "Petrified 1",
-	[16963] = "Petrified 2"
+	[727]		= "Immobilize",
+	[791]		= "Fear",
+	[872]		= "Stun",
+	[833]		= "Daze",
+	[27705]		= "Taunt",
+	[15090]		= "Petrified 1",
+	[16963]		= "Petrified 2",
 }
 
 ml_global_information.InvulnerabilityConditions = {
-	[762] = "Determined",
-	[903] = "Righteous Indignation",
-	[36143] = "Destruction Immunity",
+	[762]		= "Determined",
+	[903]		= "Righteous Indignation",
+	[36143]		= "Destruction Immunity",
 }
 
 -- Moved this here so it's easier to look at and easier to copy paste
