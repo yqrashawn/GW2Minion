788e89ca304a3218ded017108de01c3c2a423d6d
Jorith <jmzwaan@gmail.com>
2018-01-30 12:08:51 +0100

    - buy/sell/salvage manager: windows can collapse again.

--- GW2Minion/gw2_buy_manager.lua
+++ GW2Minion/gw2_buy_manager.lua
@@ -113,7 +113,7 @@ function gw2_buy_manager.mainWindow.Draw(event,ticks)
 		-- set size on first use only.
 		GUI:SetNextWindowSize(250,400,GUI.SetCond_FirstUseEver)
 		-- update visible and open variables.
-		gw2_buy_manager.mainWindow.visible, gw2_buy_manager.mainWindow.open = GUI:Begin(gw2_buy_manager.mainWindow.name, gw2_buy_manager.mainWindow.open, GUI.WindowFlags_AlwaysAutoResize+GUI.WindowFlags_NoCollapse)
+		gw2_buy_manager.mainWindow.visible, gw2_buy_manager.mainWindow.open = GUI:Begin(gw2_buy_manager.mainWindow.name, gw2_buy_manager.mainWindow.open, GUI.WindowFlags_AlwaysAutoResize) --+GUI.WindowFlags_NoCollapse
 		if (gw2_buy_manager.mainWindow.visible) then
 			-- Status field.
 			-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
